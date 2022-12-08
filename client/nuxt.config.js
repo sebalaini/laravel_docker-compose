4a61ba6926e45bd2138f1b039bff02cc53b2978a
Sebastiano <sebastianolaini@gmail.com>
2022-12-08 22:21:43 +0100

    fix vite hmr error

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -39,11 +39,9 @@ export default {
 
   vite: {
     server: {
-      strictPort: true,
-      port: 3000,
       hmr: {
         protocol: 'ws',
-        clientPort: 3000
+        host: '0.0.0.0'
       }
     },
     css: {
